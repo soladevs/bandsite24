69d92b371be38bcd9f7599011f277045e0a5b75e
sola <dariuslewiss@hotmail.co.uk>
2024-07-26 23:28:10 +0100

    routing fix attempt 1 part 2 subdivision I addenum A

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import Store from './Store';
 import Contact from './Contact';
 import Social from './Social';
 
 const App = () => {
-
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path={"/bandsite24"} element={<Home />} /> 
+          <Route path="/" element={<Home />} />
           <Route path="/store" element={<Store />} />
           <Route path="/social" element={<Social />} />
           <Route path="/contact" element={<Contact />} />
